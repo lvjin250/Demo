dfa1184fe54cc1e9c936873ab09c637033ae51fb
AuTis <61230019+lvjin250@users.noreply.github.com>
2022-04-30 21:22:00 +0800

    JavaDemo

---
+++ src/com/lvjin/util/DBConnection.java
@@ -0,0 +1,24 @@
+package com.lvjin.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    public static Connection getConn() {
+        Connection conn = null;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("数据库驱动加载成功");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/test", "root", "122333");
+            System.out.println("创建连接成功");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
